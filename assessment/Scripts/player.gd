ff8864406947507dd4bd554ed5e7bbf37ee068c4
fosterhayes-jpg <foster.hayes@cathcollege.school.nz>
2026-06-09 10:28:15 +1200

    player movement and animations changing started

--- assessment/Scripts/player.gd
+++ assessment/Scripts/player.gd
@@ -1,25 +1,33 @@
 extends CharacterBody2D
 
+const speed = 100
+var current_direction = "none"
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _physics_process(delta):
+	playermovement(delta)
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func playermovement(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		current_direction = "right"
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_direction = "left"
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		current_direction = "down"
+		velocity.y = speed
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_up"):
+		current_direction = "up"
+		velocity.y = -speed
+		velocity.x = 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
+		velocity.y = 0
+	
 	move_and_slide()
+	
+	
