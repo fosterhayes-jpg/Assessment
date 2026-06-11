6dfff9323e02fbed6d4f5a63425f505b331885f5
fosterhayes-jpg <foster.hayes@cathcollege.school.nz>
2026-06-11 21:59:28 +1200

    enemy created

---
+++ assessment/Scripts/zombie.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var speed = 80
+var player_chase = false
+var player = null
+
+func _physics_process(_delta):
+	if player_chase:
+		position += (player.position - position)/speed
+
+		$AnimatedSprite2D.play("walk")
+		
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("idle")
+
+func _on_detection_area_body_entered(body: Node2D):
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(_body: Node2D):
+	player = null
+	player_chase = false
