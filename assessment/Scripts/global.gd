12eccddde683830051ff2f5debcec0c58b037fcf
fosterhayes-jpg <foster.hayes@cathcollege.school.nz>
2026-08-10 08:37:44 +1200

    world border

--- assessment/Scripts/global.gd
+++ assessment/Scripts/global.gd
@@ -1,3 +1,11 @@
 extends Node
 
 var player_current_attack = false 
+
+var current_scene = "world" #world cliff_side
+var transition_scene = false
+
+var player_exit_cliffside_posx = 0
+var player_exit_cliffside_posy = 0
+var player_exit_cliffside_posx = 0
+var player_exit_cliffside_posy = 0
