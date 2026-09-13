35635a54b885708a41e581e6aa91f6c4c461a6b2
fosterhayes-jpg <foster.hayes@cathcollege.school.nz>
2026-09-14 09:24:38 +1200

    fixed the project settings and started the main menu

--- assessment/Scripts/cliff_side.gd
+++ assessment/Scripts/cliff_side.gd
@@ -11,8 +11,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-	
 
 
 func _on_cliffside_exitpoint_body_entered(body: Node2D) -> void:
